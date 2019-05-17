5869fc60343df0f0e2358d6a73b304e7e54371ff
sylvainBrunet <sylvain69740@gmail.com>
2019-05-17 17:38:08 +0200

    c'est bon les gars ça va aller

--- app.js
+++ app.js
@@ -32,7 +32,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  //afficher le header
+  console.log(req.headers);
+  //affiche le body
+  console.log(req.body);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
